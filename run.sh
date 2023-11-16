71a550b26bb3eb38784572a1cc67f09da59bc1f4
dandanlau <kobe.daniel@gmail.com>
2023-11-16 14:30:53 -0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Run the command and capture the output
+TDP=360000
+#set the power limit
+power_limit=400000
+half_TDP=$((TDP / 2))
+rm result.txt
+
+# Start the loop
+#while [ $power_limit -ge $half_TDP ]
+while [ $power_limit -ge 0 ]
+do
+	echo "Setting current power limit: $power_limit" >> result.txt
+	/opt/e-sms/e_smi/bin/e_smi_tool --setpowerlimit 0 $power_limit
+
+	# Use grep to find the line containing "GFLOPS" and write it to result.txt
+	output=$(sudo ./run_dgemm.py > output_tmp.txt) &
+	job_id=$!
+	sleep 10
+#	clkfreq=$(/opt/e-sms/e_smi/bin/e_smi_tool --showclocks)
+
+
+	# this will grep the core frequencof the first 'n' cores, and do an average it. 
+	clkfreq_allcore=$(grep "^[c]pu MHz" /proc/cpuinfo | head -n 8)
+	wait $job_id
+	
+
+	# Use awk to parse the frequencies and calculate their average
+	average_clkfreq=$(echo "$clkfreq_allcore" | awk -F: '{ total += $2; count++ } END { print total/count }')		
+
+	
+	grep "GFLOPS" output_tmp.txt >> result.txt
+	echo "clkfreq = $average_clkfreq"  >> result.txt
+    	# Subtract 10 from power_limit
+    	power_limit=$((power_limit - 10000))
+done
+
